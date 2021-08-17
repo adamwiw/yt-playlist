e671dd9b90dc20616a99957fd2f5ed2b28385871
Adam <a20152-1@yahoo.com>
2021-08-17 14:48:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from youtube_playlist import YoutubePlaylist
 from youtubesearchpython.internal.constants import SearchMode
 
 
-DEFAULT_YOUTUBE_STREAM_AUDIO = 140
+DEFAULT_YOUTUBE_STREAM_AUDIO = 141
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Download playlists using a query')
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('query', type=str, help='Playlist search query')
     parser.add_argument('--audio-id', type=int,
                         default=DEFAULT_YOUTUBE_STREAM_AUDIO,
-                        help='Defaults to {DEFAULT_YOUTUBE_STREAM_AUDIO}')
+                        help='Defaults to 256kbps AAC({DEFAULT_YOUTUBE_STREAM_AUDIO})')
     parser.add_argument('--search-preferences', type=str,
                         default=SearchMode.playlists,
                         help=f'Defaults to playlists({SearchMode.playlists})')
