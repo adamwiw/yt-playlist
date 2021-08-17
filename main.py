4c178334edef12102c0e80e44a0a759e4ef36799
Adam <a20152-1@yahoo.com>
2021-08-17 04:43:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     parser.add_argument('dir', type=str, help='Download Directory')
     parser.add_argument('query', type=str, help='Playlist search query')
     parser.add_argument('--audio', type=int,
-                        default=DEFAULT_YOUTUBE_STREAM_AUDIO, 
+                        default=DEFAULT_YOUTUBE_STREAM_AUDIO,
                         help='Youtube stream audio ID')
     args = parser.parse_args()
-    youtubePlaylist = YoutubePlaylist(
-        str(args.audio), path.join(args.dir, ''))
+    youtubePlaylist = YoutubePlaylist(str(args.audio),
+                                      path.join(args.dir, ''))
 
     retries = 0
     while retries < 5:
